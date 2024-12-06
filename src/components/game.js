1ceae6fed5339a3a3f9d348f1f8b276032a6a157
BeiyaC <anis.grigah@le-campus-numerique.fr>
2024-12-06 06:32:19 +0100

    improvedesign

--- src/components/game.js
+++ src/components/game.js
@@ -20,29 +20,31 @@ const Game = () => {
             {!selectedGame && (
                 <div className="buttonContainer">
                     <button
-                        className="basic-button m-5 group inline-flex items-center gap-2 rounded text-sm font-semibold transition-all hover:text-white hover:shadow-lg active:scale-95"
+                        className="basic-button m-5 group inline-flex flex-col items-center gap-2 rounded text-sm font-semibold transition-all hover:text-white hover:shadow-lg active:scale-95"
                         onClick={() => handleSelectGame("tic-tac-toe")}
                     >
-                        <span
-                            className="block rounded-sm px-6 py-2 group-hover:bg-transparent">Play TicTacToe</span>
+                        <span className="block rounded-sm px-6 py-2 group-hover:bg-transparent">Play TicTacToe</span>
+                        <span className="text-sm">(You have to choose a player to fight into this game)</span>
                     </button>
                     <button
-                        className="basic-button m-5 group inline-flex items-center gap-2 rounded text-sm font-semibold transition-all hover:text-white hover:shadow-lg active:scale-95"
+                        className="basic-button m-5 group inline-flex flex-col items-center gap-2 rounded text-sm font-semibold transition-all hover:text-white hover:shadow-lg active:scale-95"
                         onClick={() => handleSelectGame("memory")}
                     >
-                        <span
-                            className="block rounded-sm px-6 py-2 group-hover:bg-transparent">Play Memory</span>
+                        <span className="block rounded-sm px-6 py-2 group-hover:bg-transparent">Play Memory</span>
+                        <span className="text-sm">(You have to complete the game before the end of the timer)</span>
+
                     </button>
                     <button
-                        className="basic-button m-5 group inline-flex items-center gap-2 rounded text-sm font-semibold transition-all hover:text-white hover:shadow-lg active:scale-95"
+                        className="basic-button m-5 group inline-flex flex-col items-center gap-2 rounded text-sm font-semibold transition-all hover:text-white hover:shadow-lg active:scale-95"
                         onClick={handleNavigate}
                     >
-                        <span
-                            className="block rounded-sm px-6 py-2 group-hover:bg-transparent">Play Scroll</span>
+                        <span className="block rounded-sm px-6 py-2 group-hover:bg-transparent">Play Scroll</span>
+                        <span className="text-sm">(You have to choose a player to fight into this game, winner pick a bonus card)</span>
+
                     </button>
                 </div>
             )}
-            {selectedGame === "tic-tac-toe" && <TicTacToe />}
+            {selectedGame === "tic-tac-toe" && <TicTacToe/>}
             {selectedGame === "memory" && <Memory />}
         </div>
     );
--- src/components/home.js
+++ src/components/home.js
@@ -1,12 +1,14 @@
 import Header from "./header";
 import { useNavigate } from "react-router-dom";
+import challenge from "../assets/challenge.png"
+import logo from "../assets/title.png";
+import React from "react";
 
 const Home = () => {
     const navigate = useNavigate();
 
     const onButtonClick = async (page) => {
         switch (page) {
-            case "problem": await navigate("/problem"); break;
             case "quizz": await navigate("/quizz"); break;
             case "game": await navigate("/game"); break;
             default: await navigate("/");
@@ -16,10 +18,14 @@ const Home = () => {
     return (
         <div className="App">
             <Header />
-            <div className="App-content">
-                <h2>Hello mate, you are able to choose between our two different challenges.</h2>
-                <div className="">
-                    <p>You will have to answer a little question, it can be a good choice if you want to learn about the history of code.</p>
+            <div className="App-content flex-col justify-items-center">
+                <div className="justify-items-center">
+                    <h2 className="">Welcome to our Challenge Mode. You are able to choose between our two modes.</h2>
+                    <img src={challenge} className="h-20 mt-5" alt="logo"/>
+                </div>
+                <div className="mt-5">
+                    <p>You will have to answer a little question, it can be a good choice if you want to learn about the
+                        history of code.</p>
                     <div className="buttonContainer">
                         <button
                             className="basic-button group inline-flex items-center gap-2 rounded bg-gradient-to-r from-[#9e58e9] to-blue-500 p-[2px] text-sm font-semibold transition-all hover:text-white hover:shadow-lg active:scale-95 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
@@ -29,8 +35,8 @@ const Home = () => {
                         </button>
                     </div>
                 </div>
-                <div>
-                    <p>You will have to play a little game, it can be a good choice if you want to learn about the history of code.</p>
+                <div className="mt-5">
+                    <p>You will have to play a little game, no code, only fun !</p>
                     <div className="buttonContainer">
                         <button
                             className="basic-button group inline-flex items-center gap-2 rounded bg-gradient-to-r from-[#9e58e9] to-blue-500 p-[2px] text-sm font-semibold transition-all hover:text-white hover:shadow-lg active:scale-95 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
